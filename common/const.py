816ebc20c3fe621a689fec979dfd719bd0ae5dde
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2019-10-27 00:39:51 +0200

    defined some global constants

---
+++ common/const.py
@@ -0,0 +1,7 @@
+allrecipes_url_base = "https://www.allrecipes.com/recipes/79/desserts/?page={page}"
+jamie_oliver_url_base = "https://www.jamieoliver.com/recipes/category/course/desserts/?rec-page={page}"
+
+allrecipes_filename_base = "allrecipes{:03d}.html"
+jamie_oliver_filename_base = "jamie_oliver{:02d}.html"
+
+directory = "pages"
