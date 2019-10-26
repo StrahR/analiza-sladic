28474cb1a32df082d628476ba6e2137184256b62
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2019-10-27 00:42:14 +0200

    created the scraper function

---
+++ scraper/scraper.py
@@ -0,0 +1,14 @@
+from common import const, tools
+from common.tools import fmt
+
+
+def scrape(url_base, filename_base, start=1, end=None):
+    i = start-1
+    while True:
+        i += 1
+        if end and i > end:
+            break
+        text = tools.page_content(fmt(url_base, page=i))
+        if text is None:
+            break
+        tools.save(text, const.directory, fmt(filename_base, i))
